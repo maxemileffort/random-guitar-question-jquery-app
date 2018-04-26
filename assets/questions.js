6c17d1d0c878ee52c9fe1bf06eeeee47e5985a3a
Max <max.w29@hotmail.com>
2018-04-26 03:25:36 -0500

    finished questions, finished basic layout, added some js functionality

---
+++ assets/questions.js
@@ -0,0 +1,131 @@
+const quiz = [
+    {
+        id: 1,
+        question: "I wanna rock and roll all night...",
+        answerChoices: [
+            "... and sleep til noon!",
+            "... just kidding, I don't rock or roll.",
+            "... and party every day!",
+            "... I can raise and lower my cholesterol at will.",
+        ],
+        correctAnswer: "... and party every day!",
+        feedback: "I hope you got this one right, because the next few won't be so easy!"
+    },
+
+    {
+        id: 2,
+        question: 'How many strings total does a guitar usually have?',
+        answerChoices: [
+            "4",
+            "5",
+            "6",
+            "7",
+        ],
+        correctAnswer: '6',
+        feedback: "It's called 6-String Trivia for a reason!"
+    },
+
+    {
+        id: 3,
+        question: "What was Jimi Hendrix's favorite guitar?",
+        answerChoices: [
+            "Gibson Les Paul",
+            "Fender Stratocaster",
+            "Twang Machine",
+            "Gibson Explorer",
+        ],
+        correctAnswer: 'Fender Stratocaster',
+        feedback: "I really should have a tweeter account."
+    },
+
+    {
+        id: 4,
+        question: 'A guitar that usually hits the lower tones and most of the time only has 4 strings is called a...',
+        answerChoices: [
+            "broken guitar.",
+            "bass guitar.",
+            "keytar.",
+            "lower tone guitar.",
+        ],
+        correctAnswer: 'bass guitar.',
+        feedback: "Bears. Beets. Battlestar Galactica."
+    },
+
+    {
+        id: 5,
+        question: 'Time to turn it to 11! When was the guitar invented?',
+        answerChoices: [
+            "1776",
+            "2001",
+            "1492",
+            "1931",
+        ],
+        correctAnswer: '1931',
+        feedback: "Identity theft is not a joke, Jim! Millions of families suffer every year!"
+    },
+
+    {
+        id: 6,
+        question: 'Who is the lead guitarist for Led Zeppelin?',
+        answerChoices: [
+            "Jimmy Page",
+            "Slash",
+            "BB King",
+            "Paul McCartney",
+        ],
+        correctAnswer: 'Jimmy Page',
+        feedback: "Blink once if you want me to pull the plug..."
+    },
+
+    {
+        id: 7,
+        question: 'What is the standard tuning for a 6 string guitar?',
+        answerChoices: [
+            "B-B-B-B-B-B",
+            "A-B-C-D-E-F",
+            "E-A-D-G-B-E",
+            "D-A-D-G-B-E",
+        ],
+        correctAnswer: "E-A-D-G-B-E",
+        feedback: "Ryan started the fire!"
+    },
+
+    {
+        id: 8,
+        question: 'If you can hear the bass player...',
+        answerChoices: [
+            "... and sleep til noon!",
+            "... just kidding, I don't rock or roll.",
+            "... and party every day!",
+            "... then he's playing too loud!",
+        ],
+        correctAnswer: "... then he's playing too loud!",
+        feedback: "You couldn't handle my undivided attention."
+    },
+
+    {
+        id: 9,
+        question: "What part of the electric guitar picks up the noise from the strings and sends to to the amp?",
+        answerChoices: [
+            "The bridge",
+            "The fret",
+            "The pickups",
+            "The body",
+        ],
+        correctAnswer: 'The pickups',
+        feedback: "That's why they call it murder and not muckduck."
+    },
+
+    {
+        id: 10,
+        question: 'What show were the random quotes from?',
+        answerChoices: [
+            "The Office",
+            "Roseanne",
+            "How I Met Your Mother",
+            "... Dwight, you ignorant slut!",
+        ],
+        correctAnswer: 'The Office',
+        feedback: "Survey says... The Office!"
+    }
+];
--- main.js
+++ main.js
@@ -1,3 +1,103 @@
-import quiz from 'questions.js';
 
-let count = 0;
+
+let count = 0; //used to render question with id === count
+let numCorrect = 0;
+let chosenAnswer = '';
+
+
+//button behaviors
+$('#start').on('click', ()=>{
+    //should hide the start page and then render 
+    //the question box, answers, and footer
+    count = 1;
+    hideEverything();
+    startQuiz();
+});
+
+$('#check').on('click', (event)=>{
+    event.preventDefault();
+    //should check answerchoice vs correct answer and then display feedback
+    //should also add to correct counter if appropriate
+    checkAnswer();
+    
+});
+
+$('#close').on('click', (event) => {
+    event.preventDefault();
+    //should hide feedback and enable next button
+
+});
+
+$('#next').on('click', (event) => {
+    event.preventDefault();
+    count++;
+    //should re-render question box with next question and disable itself
+    //should also reflect progress in progress bar at bottom and counter
+    //at top
+});
+
+$('#reset').on('click', (event) => {
+    event.preventDefault();
+    count = 0;
+    //should take us back to start page
+});
+
+function hideEverything(){
+    //use this as a baseline to make sure that nothing is showing,
+    //and then add what pieces need to show, one at a time
+    $('div').addClass('hide');
+};
+
+function landOnPage() {
+    //add pieces that render start page
+    $('.logo').removeClass('hide');
+    $('.start-page').removeClass('hide');
+    $('.credits').removeClass('hide');
+
+};
+
+function startQuiz() {
+    //render question box, answer choices, footer and reset header
+    $('.logo').removeClass('hide');
+    $('.counter').removeClass('hide');
+    $('.correct-counter').removeClass('hide');
+    $('.question').removeClass('hide');
+    $('.answers').removeClass('hide');
+    $('.credits').removeClass('hide');
+    renderNextQuestion();
+
+};
+
+function checkAnswer(){
+    //store answer choice as a value and then check against correct answer in
+    //object. if answers match, add 1 to numCorrect. afterwards, render feedback
+
+    //for rendering feedback, page should darken a little 
+    //and feedback modal should appear with grpahic depending 
+    //on if they got it right or wrong
+    
+};
+
+function closeFeedback(){
+    //when user clicks close button on feedback modal, feedback should disappear
+    //and page lighten up
+};
+
+function renderNextQuestion(){
+    //change inner html to reflect question with id === count
+    //in question box and answer choices. 
+    //also make sure counters are working
+};
+
+function initiateApp(){
+    //hide everything and load all the functions
+    //not sure if this step in necessary
+    startQuiz();
+    checkAnswer();
+    closeFeedback();
+    renderNextQuestion();
+    hideEverything();
+    landOnPage();
+};
+
+$(initiateApp);
